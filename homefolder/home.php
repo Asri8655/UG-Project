09f531bc94d51d74e0af55e816fe56e681fde41c
Asri8655 <55702814+Asri8655@users.noreply.github.com>
2020-05-28 12:23:51 +0530

    Home page

---
+++ homefolder/home.php
@@ -0,0 +1,191 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+	<html xmlns="http://www.w3.org/1999/xhtml">
+		<head style="margin-top:100px;">
+		<link rel="stylesheet" type="text/css" href="C:\AppServ\www\project\questionbank1\css\color.css">
+			<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+			<title></title>
+			<meta name="keywords" content="" />
+			<meta name="description" content="" />
+
+			<link href="tooplate_style.css" rel="stylesheet" type="text/css" />
+			<script type="text/JavaScript" src="js/jquery-1.6.3.js"></script> 
+			<link rel="stylesheet" type="text/css" href="css/ddsmoothmenu.css" />
+			<script type="text/javascript" src="js/ddsmoothmenu.js"></script>
+			<link rel="stylesheet" href="css/slimbox2.css" type="text/css" media="screen" /> 
+			<script type="text/JavaScript" src="js/slimbox2.js"></script> 
+			<link rel="stylesheet" href="css/nivo-slider.css" type="text/css" media="screen" />
+			<style type="text/css">
+*,
+*:before,
+*:after {
+   box-sizing: border-box;
+}
+form {
+  border: 1px solid #c6c7cc;
+  border-radius: 6px;
+  font: 14px/1.4 "Helvetica Neue", Helvetica, Arial, sans-serif;
+  overflow: hidden;
+  width: 300px;
+}
+fieldset {
+  border: 0;
+  margin: 0;
+  padding: 0;
+}
+input {
+  border-radius: 5px;
+  font: 14px/1.4 "Helvetica Neue", Helvetica, Arial, sans-serif;
+  margin: 0;
+}
+.account-info {
+  padding: 20px 20px 0 20px;
+}
+.account-info label {
+  color: #FFF;
+  display: block;
+  font-weight: bold;
+  margin-bottom: 20px;
+}
+.account-info input,select {
+  background: #fff;
+  border: 1px solid #c6c7cc;
+   box-shadow: inset 0 1px 1px rgba(0, 0, 0, .1);
+  color: #636466;
+  padding: 2px;
+  margin-top: 6px;
+  width: 150;
+}
+.account-action {
+  background: #f0f0f2;
+  border-top: 1px solid #c6c7cc;
+  padding: 20px;
+align:center;
+}
+.account-action .btn {
+  background: linear-gradient(#49708f, #293f50);
+  border: 0;
+  color: #fff;
+  cursor: pointer;
+  font-weight: bold;
+  float: center;
+  padding: 8px 16px;
+}
+.account-action label {
+  color: #7c7c80;
+  font-size: 12px;
+  float: center;
+  margin: 10px 0 0 20px;
+}
+ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color:#FFFFFF;
+	Width:1150px;
+}
+
+li {
+    float: left;
+}
+
+li a {
+    display: block;
+    color: white;
+    text-align: center;
+    padding: 16px 16px;
+    text-decoration: none;
+	font-size:16px;
+	font-family:Arial, Helvetica, sans-serif;
+	font-style:normal;
+	font-weight:bold;
+}
+
+/* Change the link color to #111 (black) on hover */
+li a:hover {
+    background-color:#B2BEB5;
+}
+.active {
+    background-color:#B2BEB5;
+}
+.legcolor{
+color:#0247FE;
+}
+
+table.gridtable {
+	font-family: verdana,arial,sans-serif;
+	font-size:12px;
+	color:#666666;
+	border-width: 1px;
+	border-color: #a9c5ac;
+	border-collapse: collapse;
+}
+table.gridtable th {
+	border-width: 1px;
+	padding: 8px;
+	border-style: solid;
+	border-color: #666666;
+	background-color:#9CF;
+}
+table.gridtable td {
+	border-width: 1px;
+	padding: 8px;
+	border-style: solid;
+	border-color: #666666;
+	background-color: #ffffff;
+}
+</style>
+<script type="text/javascript">
+var image1 = new Image()
+image1.src = "images/slider/1.png"
+var image2 = new Image()
+image2.src = "images/slider/2.png"
+var image3 = new Image()
+image3.src = "images/slider/3.png"
+</script>
+	</head>
+	<body bgcolor="#6699cc">
+<div align="center">
+ <table>
+    <tr>
+   
+    <td>
+    <!img src="images/slider/2.png" alt="" width="130" height="130" name="slide"/>
+			
+			
+					<!img src="images/slider/1.png" alt="" width="750" height="130" name="slide"/>
+                    </td>
+                    </tr>
+                    </table><br/>
+ <ul>
+  <li class="active"><a href="home.php">Home</a></li>
+      <li><a href="admin.php">Admin</a></li>
+    <li><a href="stafflogin.php">Staff</a></li>
+      <li><a href="studentlogin.php">Student</a></li>
+ </ul> 
+<br/>
+<br/>
+	
+    <br/>
+<br/>
+	<div align="center">		
+  
+					<!--	<script type="text/javascript">
+        var step=1;
+        function slideit()
+        {
+            document.images.slide.src = eval("image"+step+".src");
+            if(step<2)
+                step++;
+            else
+                step=1;
+            setTimeout("slideit()",1500);
+        }
+        slideit();
+</script>
+	 -->
+		</div> 
+		
+		<!-- END of slider -->
+        </body>
+        </html>
